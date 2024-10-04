78d06a803503bf8c1c19244e2742057157cb9072
Karam-Salama <karamslama244@gmail.com>
2024-10-04 22:22:25 +0300

    just test connection

--- lib/features/Home/screens/home_screen.dart
+++ lib/features/Home/screens/home_screen.dart
@@ -5,8 +5,10 @@ class HomeScreen extends StatelessWidget {
   static const String routeName = '/homeScreen';
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Home Screen'),),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+      ),
     );
   }
 }
